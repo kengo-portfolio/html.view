8d9f1b354e9df72254f9455d8f932abbd1a048d1
kengo-portfolio <96458993+kengo-portfolio@users.noreply.github.com>
2021-12-21 17:00:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $(window).on('scroll', function (){
     var scrollPos = $(window).scrollTop();
     var wh = $(window).height();
 
-    if(scrollPos > elemOffset - wh + (wh / ) ){
+    if(scrollPos > elemOffset - wh + (wh / 3) ){
       $(this).addClass(isAnimate);
     }
     
